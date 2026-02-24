36cc55e7dff75e66cd28d5ee4ad0e55cb959b596
Shobana-Christy <shobichristy07@gmail.com>
2026-02-24 13:55:25 -0600

    Clean up WeatherService by removing commented code
    Removed commented-out code for WeatherService class.

--- src/main/java/com/example/myproject/service/WeatherService.java
+++ src/main/java/com/example/myproject/service/WeatherService.java
@@ -1,35 +1,3 @@
-//package com.example.myproject.service;
-//import org.json.JSONObject;
-//
-//import com.example.myproject.Weather;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.client.RestTemplate;
-//
-//@Service
-//public class WeatherService {
-//
-//    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//    // https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
-//    // https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
-//
-//    private String API_KEY = "";
-//
-//    public Weather getCurrentWeather(String city) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
-//        String json = restTemplate.getForObject(url, String.class);
-//        JSONObject obj = new JSONObject(json);
-//
-//        String cityName = obj.getString("name");
-//        double temp = obj.getJSONObject("main").getDouble("temp");
-//        String description = obj.getJSONArray("weather")
-//                .getJSONObject(0)
-//                .getString("description");
-//        return new Weather(cityName, String.valueOf(temp), description);
-//    }
-//
-//}
-
 package com.example.myproject.service;
 
 import com.example.myproject.Weather;
